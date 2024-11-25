0dc81548189294db54742c58592b4ceb7795fcfb
Карпенко Глеб <glebkarpenko1@gmail.com>
2024-11-25 21:43:47 +0200

    add compress module

--- image_tools/main.py
+++ image_tools/main.py
@@ -1,5 +1,5 @@
 import argparse
-from image_tools import crop_module
+from image_tools import crop_module, compress_module
 
 def main():
     parser = argparse.ArgumentParser(
@@ -37,10 +37,42 @@ def main():
         help="Output folder for cropped images. Default is '../cropped'."
     )
 
+    # Compress command
+    compress_parser = subparsers.add_parser(
+        "compress",
+        help = "Compress images in a folder based on desired width and quality."
+    )
+    compress_parser.add_argument(
+        "--input_folder",
+        type=str,
+        default="../to_compress",
+        help="Input folder containing images to compress. Default is '../to_compress'."
+    )
+    compress_parser.add_argument(
+        "--output_folder",
+        type=str,
+        default="../compressed",
+        help="Output folder for cropped images. Default is 'compressed'."
+    )
+    compress_parser.add_argument(
+        "--width",
+        type=int,
+        default=720,
+        help="Width for result image. Height is determined respectivly to aspect ratio."
+    )
+    compress_parser.add_argument(
+        "--quality",
+        type=int,
+        default=80,
+        help="Desired quality for result image. Bigger value means higher quality. Defaul is 80."
+    )
+
     args = parser.parse_args()
 
     if args.command == "crop":
         crop_module.main(args)
+    if args.command == "compress":
+        compress_module.main(args)
 
 if __name__ == "__main__":
     main()
